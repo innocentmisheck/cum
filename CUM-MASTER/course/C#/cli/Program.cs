590d14c6e2f6974e7332e6215cbbf800521fcf13
= <innocentmisheck03@gmail.com>
2023-07-19 00:29:26 +0200

    cum-cli updates

--- CUM-MASTER/course/C#/cli/Program.cs
+++ CUM-MASTER/course/C#/cli/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Gammis
+{
+    public class RefCodeGenerator
+    {
+        private static readonly Random random = new Random();
+        private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public string GenerateRefCode(int length)
+        {
+            var refCodeBuilder = new System.Text.StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                refCodeBuilder.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return refCodeBuilder.ToString();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int refCodeLength = 10; // Length of the ref_code
+            RefCodeGenerator refCodeGenerator = new RefCodeGenerator();
+
+            while (true)
+            {
+                string refCode = refCodeGenerator.GenerateRefCode(refCodeLength);
+
+                // Change the console text color to green for the label text
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Generated ref_code: ");
+
+                // Change the console text color to red for the refCode itself
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(refCode);
+
+                // Reset the console text color to default
+                Console.ResetColor();
+
+                Console.WriteLine("Press Enter to generate another refcode or type 'exit' to quit.");
+
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                    break;
+            }
+        }
+    }
+}
